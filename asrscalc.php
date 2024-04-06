e6bba5a5d65e0a6eaefd32f34e1e908903df317b
rolfmblindgren <rolf@grendel.no>
2024-04-06 19:12:45 +0200

    justerer litt til

--- asrscalc.php
+++ asrscalc.php
@@ -24,7 +24,7 @@ $kryssfarge = imagecolorallocate($image, 0, 0, 0);
 
 
 // Startposisjoner for svarfeltene
-$startX = 1504; // Start X-posisjon for det første spørsmålet
+$startX = 1404; // Start X-posisjon for det første spørsmålet
 $startY = 578; // Start Y-posisjon for det første spørsmålet
 $deltaY = 40;  // Avstanden vertikalt mellom hvert svarfelt
 
