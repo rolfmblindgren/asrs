3f297b32a336ff1e022d6bff199e640242561415
rolfmblindgren <rolf@grendel.no>
2024-04-06 21:21:48 +0200

    mer delta y

--- asrscalc.php
+++ asrscalc.php
@@ -21,7 +21,7 @@ $kryssfarge = imagecolorallocate($image, 0, 0, 0);
 // Startposisjoner for svarfeltene
 $baseStartX = 1191; // Start X-posisjon for det første spørsmålet
 $baseStartY = 578; // Start Y-posisjon for det første spørsmålet
-$deltaY = 70;  // Avstanden vertikalt mellom hvert svarfelt
+$deltaY = 80;  // Avstanden vertikalt mellom hvert svarfelt
 $deltaX = 76;
 
 // Avstanden horisontalt mellom hver kolonne basert på skjemaet
