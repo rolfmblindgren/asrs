bf62a9d69fe6dfd7ad4c76154f8d84b6204d9721
rolfmblindgren <rolf@grendel.no>
2024-04-06 21:28:58 +0200

    mer delta x

--- asrscalc.php
+++ asrscalc.php
@@ -25,7 +25,7 @@ $deltaY = 81.5;  // Avstanden vertikalt mellom hvert svarfelt
 $deltaX = 76;
 
 // Avstanden horisontalt mellom hver kolonne basert på skjemaet
-$avstandKolonne = 87;
+$avstandKolonne = 90;
 
 // Størrelse på krysset
 $kryssStørrelse = 10;
