be38db4ec9c13c8ddfb027c761137d3d67a02c5b
rolfmblindgren <rolf@grendel.no>
2024-04-06 17:38:54 +0200

    First test

---
+++ asrscalc.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/home/vds/www/vendor/autoload.php';
+
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Bridge\Google\Transport\GmailSmtpTransport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Address;
+
+phpinfo();
+
+?>
